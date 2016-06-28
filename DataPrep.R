235ad1814cd0b3c3c3100f513c9ee3a19ab5b8af
Jothsna Rajan <jothsnarajan@gmail.com>
2016-06-28 14:43:23 +0530

    v1.2
    Cleaning up the SSA Dataset - Complete

--- DataPrep.R
+++ DataPrep.R
@@ -1,137 +1,59 @@
 #Everything that exists is an object.
 #Everything that happens is a function call.
 
+setwd("D:/Share Point/OneDrive/Comprehensive Exam - Data files")
+
 library(foreign)
 library(stargazer)
 library(plyr)
 library(reshape)
 
-setwd("D:/Share Point/OneDrive/Comprehensive Exam - Data files")
-Files <- list.files(pattern = "*.csv")
 options(scipen = 999, digits=2)
+Files <- list.files(pattern = "*.csv")
 
+#Initializing Functions
 na.zero <- function (x) {
-  x[is.na(x)] <- 0
+  x[is.na(x)] <- 999
   return(x)
 }
 
-#Reading SSA Data
-Data <- ldply(lapply(Files[1], read.csv))
-k <- c(1, 4, 8, 9, 15, 18, 20, 22, 23, 28:33, 36, 44:49, 51, 55:62)
-Data <- Data[, k]
-
-#School Categories with Secondary Level
-category <- c(3, 5)
+category <- c(3, 5, 6, 7, 8, 10)
 manage <- c(1, 2, 3, 4)
 taluk <- c("AFZALPUR", "ALAND", "BAGEPALLY", "BANGARAPETE", "CHANNAPATNA", "CHIKKABALLAPUR", "CHINCHOLI", "CHINTAMANI", "CHITTAPUR", "DEVANAHALLI", "DODDABALLAPURA", "GOWRIBIDANUR", "GUDIBANDA", "GULBARGA NORTH", "GULBARGA SOUTH", "HOSAKOTE", "JEWARGI", "K G F", "KANAKAPURA", "KOLAR", "MAGADI", "MALUR", "MULBAGAL", "NELAMANGALA", "RAMANAGARA", "SEDAM", "SHAHAPUR", "SHORAPUR", "SIDLAGHATTA", "SRINIVASAPUR", "YADGIR")
 
-Data <- droplevels(subset(Data, eval(is.element(Data$Sch_Category, category))))# & is.element(Data$Sch_Management, manage))))
-Data[Data$Toilet_Girls == 2,17] = 0
-Data[Data$Electricity == 2, 18] = 0
-Data[Data$Book_Bank == 2, 20] = 0
-Data[Data$PlayGround == 2, 21] = 0
+k <- data.frame(matrix(0, nrow = 29, ncol = 9))
+k[,1:4] <- c(1, 4, 8, 9, 15, 18, 20, 22, 23, 28:31, 36, 44:49, 51, 55:62)
+k[,5] <- c(2, 4, 9, 10, 16, 19, 21, 23, 24, 29:32, 37, 46, 51:55, 57, 62:69)
+k[,6] <- c(2, 4, 9, 10, 16, 19, 21, 23, 24, 29:32, 37, 46, 51:55, 57, 61:68)
+k[,7] <- c(1, 4, 9, 10, 16, 19, 21, 23, 24, 29:32, 36, 45, 50:54, 56, 60:67)
+k[,8] <- c(2, 4, 9, 10, 16, 19, 21, 23, 24, 29:32, 37, 46, 51:55, 57, 61:68)
+
+Data <- lapply(Files[1:8], read.csv)
+
+for(i in 1:8){
+  Data[[i]] <- Data[[i]][,k[,i]]
+  colnames(Data[[i]]) <- c("School_Code", "Block_Name", "Rural_Urban", "Medium", "Sch_Management", "Sch_Category", "School_Type", "No_of_Working_Days", "No_of_Acad_Inspection", "School_Dev_Grant_Recd", "School_Dev_Grant_Expnd", "TLM_Grant_Recd", "TLM_Grant_Expnd", "Tot_Clrooms", "Toilet_Girls", "Electricity", "Boundary_Wall", "Library", "PlayGround", "Blackboard", "Drinking_Water", "Male_Tch", "Female_Tch", "NoResp_Tch", "Head_Teacher", "Graduate_Teachers", "Tch_with_Prof_Qual", "Days_involved_in_non_tch_assgn", "Teachers_involved_in_non_tch_assgn")
+  Data[[i]] <- na.zero(Data[[i]])
+  Data[[i]]$acyear <- i
+}
+
+Data <- ldply(Data)
+Data <- droplevels(subset(Data, eval(is.element(Data$Sch_Category, category))))
+Data[Data$Toilet_Girls >= 1, 15] = 1
+Data[eval(Data$Electricity == 2 | Data$Electricity == 3), 16] = 0
+Data[Data$Library == 2, 18] = 0
+Data[Data$PlayGround == 2, 19] = 0
 Data[Data$Rural_Urban == 2, 3] = 0
 Data$Public <- rep(0, nrow(Data))
-Data[is.element(Data$Sch_Management, manage), 32] = 1
+Data[is.element(Data[,5], manage), 31] = 1
 Data$Split <- rep(0, nrow(Data))
-Data[is.element(Data$Block_Name, taluk), 33] = 1
+Data[is.element(Data[,2], taluk), 32] = 1
 
-Data <- Data[,-c(1, 4:7, 19, 22, 23, 26, 27)]
-ssa.05 <- aggregate(Data, by = list(Data$Block_Name), mean)[,-2]
-colnames(ssa.05)[1] <- "taluk"
 
-Data <- ldply(lapply(Files[2], read.csv))
-k <- c(1, 4, 8, 9, 15, 18, 20, 22, 23, 28:33, 36, 44:49, 51, 55:62)
-Data <- Data[, k]
+#Set up to Aggregate
+Data <- Data[,-c(1, 4:7, 17, 20, 21, 24, 25)]
+Dataset <- aggregate(Data, by = list(Data$Block_Name, Data$acyear), mean)[,-c(2, 3)]
+colnames(Dataset)[1] <- "taluk"
 
-#School Categories with Secondary Level
-category <- c(3, 5)
-manage <- c(1, 2, 3, 4)
-taluk <- c("AFZALPUR", "ALAND", "BAGEPALLY", "BANGARAPETE", "CHANNAPATNA", "CHIKKABALLAPUR", "CHINCHOLI", "CHINTAMANI", "CHITTAPUR", "DEVANAHALLI", "DODDABALLAPURA", "GOWRIBIDANUR", "GUDIBANDA", "GULBARGA NORTH", "GULBARGA SOUTH", "HOSAKOTE", "JEWARGI", "K G F", "KANAKAPURA", "KOLAR", "MAGADI", "MALUR", "MULBAGAL", "NELAMANGALA", "RAMANAGARA", "SEDAM", "SHAHAPUR", "SHORAPUR", "SIDLAGHATTA", "SRINIVASAPUR", "YADGIR")
-
-Data <- droplevels(subset(Data, eval(is.element(Data$Sch_Category, category))))# & is.element(Data$Sch_Management, manage))))
-Data[Data$Toilet_Girls == 2,17] = 0
-Data[Data$Electricity == 2, 18] = 0
-Data[Data$Book_Bank == 2, 20] = 0
-Data[Data$PlayGround == 2, 21] = 0
-Data[Data$Rural_Urban == 2, 3] = 0
-Data$Public <- rep(0, nrow(Data))
-Data[is.element(Data$Sch_Management, manage), 32] = 1
-Data$Split <- rep(0, nrow(Data))
-Data[is.element(Data$Block_Name, taluk), 33] = 1
-
-Data <- Data[,-c(1, 4:7, 19, 22, 23, 26, 27)]
-ssa.06 <- aggregate(Data, by = list(Data$Block_Name), mean)[,-2]
-colnames(ssa.06)[1] <- "taluk"
-
-Data <- ldply(lapply(Files[3], read.csv))
-k <- c(1, 4, 8, 9, 15, 18, 20, 22, 23, 28:33, 36, 44:49, 51, 55:62)
-Data <- Data[, k]
-
-#School Categories with Secondary Level
-category <- c(3, 5)
-manage <- c(1, 2, 3, 4)
-taluk <- c("AFZALPUR", "ALAND", "BAGEPALLY", "BANGARAPETE", "CHANNAPATNA", "CHIKKABALLAPUR", "CHINCHOLI", "CHINTAMANI", "CHITTAPUR", "DEVANAHALLI", "DODDABALLAPURA", "GOWRIBIDANUR", "GUDIBANDA", "GULBARGA NORTH", "GULBARGA SOUTH", "HOSAKOTE", "JEWARGI", "K G F", "KANAKAPURA", "KOLAR", "MAGADI", "MALUR", "MULBAGAL", "NELAMANGALA", "RAMANAGARA", "SEDAM", "SHAHAPUR", "SHORAPUR", "SIDLAGHATTA", "SRINIVASAPUR", "YADGIR")
-
-Data <- droplevels(subset(Data, eval(is.element(Data$Sch_Category, category))))# & is.element(Data$Sch_Management, manage))))
-Data[Data$Toilet_Girls == 2,17] = 0
-Data[Data$Electricity == 2, 18] = 0
-Data[Data$Book_Bank == 2, 20] = 0
-Data[Data$PlayGround == 2, 21] = 0
-Data[Data$Rural_Urban == 2, 3] = 0
-Data$Public <- rep(0, nrow(Data))
-Data[is.element(Data$Sch_Management, manage), 32] = 1
-Data$Split <- rep(0, nrow(Data))
-Data[is.element(Data$Block_Name, taluk), 33] = 1
-
-Data <- Data[,-c(1, 4:7, 19, 22, 23, 26, 27)]
-ssa.07 <- aggregate(Data, by = list(Data$Block_Name), mean)[,-2]
-colnames(ssa.07)[1] <- "taluk"
-
-Data <- ldply(lapply(Files[4], read.csv))
-k <- c(1, 4, 8, 9, 15, 18, 20, 22, 23, 28:33, 36, 44:49, 51, 55:62)
-Data <- Data[, k]
-
-#School Categories with Secondary Level
-category <- c(3, 5)
-manage <- c(1, 2, 3, 4)
-taluk <- c("AFZALPUR", "ALAND", "BAGEPALLY", "BANGARAPETE", "CHANNAPATNA", "CHIKKABALLAPUR", "CHINCHOLI", "CHINTAMANI", "CHITTAPUR", "DEVANAHALLI", "DODDABALLAPURA", "GOWRIBIDANUR", "GUDIBANDA", "GULBARGA NORTH", "GULBARGA SOUTH", "HOSAKOTE", "JEWARGI", "K G F", "KANAKAPURA", "KOLAR", "MAGADI", "MALUR", "MULBAGAL", "NELAMANGALA", "RAMANAGARA", "SEDAM", "SHAHAPUR", "SHORAPUR", "SIDLAGHATTA", "SRINIVASAPUR", "YADGIR")
-
-Data <- droplevels(subset(Data, eval(is.element(Data$Sch_Category, category))))# & is.element(Data$Sch_Management, manage))))
-Data[Data$Toilet_Girls == 2,17] = 0
-Data[Data$Electricity == 2, 18] = 0
-Data[Data$Book_Bank == 2, 20] = 0
-Data[Data$PlayGround == 2, 21] = 0
-Data[Data$Rural_Urban == 2, 3] = 0
-Data$Public <- rep(0, nrow(Data))
-Data[is.element(Data$Sch_Management, manage), 32] = 1
-Data$Split <- rep(0, nrow(Data))
-Data[is.element(Data$Block_Name, taluk), 33] = 1
-
-Data <- Data[,-c(1, 4:7, 19, 22, 23, 26, 27)]
-ssa.08 <- aggregate(Data, by = list(Data$Block_Name), mean)[,-2]
-colnames(ssa.08)[1] <- "taluk"
-
-Data <- ldply(lapply(Files[5], read.csv))
-k <- c(2, 4, 9, 10, 16, 19, 21, 23, 24, 29:34, 37, 46, 51:55, 57, 62:69)
-Data <- Data[, k]
-
-#School Categories with Secondary Level
-category <- c(3, 5)
-manage <- c(1, 2, 3, 4)
-taluk <- c("AFZALPUR", "ALAND", "BAGEPALLY", "BANGARAPETE", "CHANNAPATNA", "CHIKKABALLAPUR", "CHINCHOLI", "CHINTAMANI", "CHITTAPUR", "DEVANAHALLI", "DODDABALLAPURA", "GOWRIBIDANUR", "GUDIBANDA", "GULBARGA NORTH", "GULBARGA SOUTH", "HOSAKOTE", "JEWARGI", "K G F", "KANAKAPURA", "KOLAR", "MAGADI", "MALUR", "MULBAGAL", "NELAMANGALA", "RAMANAGARA", "SEDAM", "SHAHAPUR", "SHORAPUR", "SIDLAGHATTA", "SRINIVASAPUR", "YADGIR")
-
-Data <- droplevels(subset(Data, eval(is.element(Data$Sch_Category, category))))# & is.element(Data$Sch_Management, manage))))
-Data[Data$Toilet_Girls_YN >= 1, 17] = 1
-Data[eval(Data$Electricity == 2 | Data$Electricity == 3), 18] = 0
-Data[Data$Book_Bank == 2, 20] = 0
-Data[Data$PlayGround == 2, 21] = 0
-Data[Data$Rural_Urban == 2, 3] = 0
-Data$Public <- rep(0, nrow(Data))
-Data[is.element(Data$Sch_Management, manage), 32] = 1
-Data$Split <- rep(0, nrow(Data))
-Data[is.element(Data$Block_Name, taluk), 33] = 1
-
-Data <- Data[,-c(1, 4:7, 19, 22, 23, 26, 27)]
-ssa.09 <- aggregate(Data, by = list(Data$Block_Name), mean)[,-2]
-colnames(ssa.09)[1] <- "taluk"
+setwd("D:/Share Point/OneDrive/GitHub/greek-salad")
+write.csv(Data, "ssa.csv")
