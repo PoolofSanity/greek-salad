1292ca0fce21d11f1203a556cc2068ec61cb7832
Jothsna Rajan <jothsnarajan@gmail.com>
2016-06-28 15:55:47 +0530

    v1.2

--- DataPrep.R
+++ DataPrep.R
@@ -17,9 +17,17 @@ na.zero <- function (x) {
   return(x)
 }
 
+Data <- lapply(Files[1:8], read.csv)
+taluk.all <- list()
+for (i in 1:8){
+  taluk.all[[i]] <- Data[[i]][,4]
+}
+taluk.all <- Reduce(intersect, taluk.all)
+
 category <- c(3, 5, 6, 7, 8, 10)
 manage <- c(1, 2, 3, 4)
 taluk <- c("AFZALPUR", "ALAND", "BAGEPALLY", "BANGARAPETE", "CHANNAPATNA", "CHIKKABALLAPUR", "CHINCHOLI", "CHINTAMANI", "CHITTAPUR", "DEVANAHALLI", "DODDABALLAPURA", "GOWRIBIDANUR", "GUDIBANDA", "GULBARGA NORTH", "GULBARGA SOUTH", "HOSAKOTE", "JEWARGI", "K G F", "KANAKAPURA", "KOLAR", "MAGADI", "MALUR", "MULBAGAL", "NELAMANGALA", "RAMANAGARA", "SEDAM", "SHAHAPUR", "SHORAPUR", "SIDLAGHATTA", "SRINIVASAPUR", "YADGIR")
+exclude <- c("SOUTH1", "ANEKAL", "SOUTH2", "SOUTH3", "SOUTH4", "NORTH4", "NORTH1", "NORTH2", "NORTH3")
 
 k <- data.frame(matrix(0, nrow = 29, ncol = 9))
 k[,1:4] <- c(1, 4, 8, 9, 15, 18, 20, 22, 23, 28:31, 36, 44:49, 51, 55:62)
@@ -27,8 +35,6 @@ k[,5] <- c(2, 4, 9, 10, 16, 19, 21, 23, 24, 29:32, 37, 46, 51:55, 57, 62:69)
 k[,6] <- c(2, 4, 9, 10, 16, 19, 21, 23, 24, 29:32, 37, 46, 51:55, 57, 61:68)
 k[,7] <- c(1, 4, 9, 10, 16, 19, 21, 23, 24, 29:32, 36, 45, 50:54, 56, 60:67)
 k[,8] <- c(2, 4, 9, 10, 16, 19, 21, 23, 24, 29:32, 37, 46, 51:55, 57, 61:68)
-
-Data <- lapply(Files[1:8], read.csv)
 
 for(i in 1:8){
   Data[[i]] <- Data[[i]][,k[,i]]
@@ -39,6 +45,8 @@ for(i in 1:8){
 
 Data <- ldply(Data)
 Data <- droplevels(subset(Data, eval(is.element(Data$Sch_Category, category))))
+Data <- droplevels(subset(Data, eval(is.element(Data$Block_Name, taluk.all))))
+Data <- droplevels(subset(Data, eval(is.element(Data$Block_Name, exclude) == 0)))
 Data[Data$Toilet_Girls >= 1, 15] = 1
 Data[eval(Data$Electricity == 2 | Data$Electricity == 3), 16] = 0
 Data[Data$Library == 2, 18] = 0
@@ -56,4 +64,4 @@ Dataset <- aggregate(Data, by = list(Data$Block_Name, Data$acyear), mean)[,-c(2,
 colnames(Dataset)[1] <- "taluk"
 
 setwd("D:/Share Point/OneDrive/GitHub/greek-salad")
-write.csv(Data, "ssa.csv")
+write.csv(Dataset, "ssa.csv")
