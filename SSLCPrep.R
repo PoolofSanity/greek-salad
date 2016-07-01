1b39c2d196142f32cdc6353908ab1b2c9cae14ff
Jothsna Rajan <jothsnarajan@gmail.com>
2016-07-01 12:29:27 +0530

    v1.3

--- SSLCPrep.R
+++ SSLCPrep.R
@@ -19,26 +19,22 @@ na.zero <- function (x) {
   return(x)
 }
 
-Data <- list()
 k <- data.frame(matrix(0, nrow = 9, ncol = 9))
 k[,1:8] <- c(4, 23, 31, 39, 47, 58, 66, 81, 82)
 k[,9] <- c(6, 28, 32, 34, 37, 40, 43, 19, 20)
 
 Year <- c(2005:2013)
+exclude <- c(0:150)
 
-for(i in 1){
-  Data <- lapply(Files[i], read.csv)
-  Data[[i]] <- Data[[i]][,k[,i]]
-  colnames(Data[[i]]) <- c("School_Code", "Marks1", "Marks2", "Marks3", "Marks4", "Marks5", "Marks6", "Total", "Result")
-  Data[[i]] <- aggregate(Data[[i]], by = list(Data[[i]]$School_Code), mean, na.action = na.omit)
-  #Data[[i]]$acyear <- Year[i]
-  }
+Data <- ldply(lapply(Files[1], read.csv))
+Data <- Data[,k[,1]]
+colnames(Data) <- c("School.Code", "Marks1", "Marks2", "Marks3", "Marks4", "Marks5", "Marks6", "Total", "Result")
+Data <- na.zero(Data)
+test <- Data
+for(i in 2:7){
+  test <- droplevels(subset(test, eval(is.element(test[,i], exclude))))  
+}
 
-#setting up a table to match schoolcodes with taluks
-#lookup <- unique(Data[[9]][,c(1, 3)])
+AggData <- aggregate(test, by = list(test$School.Code), mean)[,-2]
+colnames(AggData)[1] <- "School.Code"
 
-Data <- ldply(Data)
-#Data <- aggregate(Data, by = list(Data$School_Code), mean)[,-2]
-#colnames(Data)[1] <- "School_Code"
-
-#colnames(ldply(lapply(Files[3], read.csv)))
