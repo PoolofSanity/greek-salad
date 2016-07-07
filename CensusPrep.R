7622c169f10ad53ff4daaddf131b4f5ec68797ac
Jothsna Rajan <jothsnarajan@gmail.com>
2016-07-08 02:48:27 +0530

    v2.1

--- CensusPrep.R
+++ CensusPrep.R
@@ -3,6 +3,8 @@
 #Everything that exists is an object.
 #Everything that happens is a function call.
 
+##*****OK! This file is a mess. But it works. Well, it worked. Not so sure anymore.*****##
+
 setwd("D:/Share Point/OneDrive/Comprehensive Exam - Data files/Census")
 
 library(foreign)
@@ -15,19 +17,25 @@ library(DataCombine)
 options(scipen = 999, digits=2)
 Files <- list.files(pattern = "*.csv")
 
-Block <- ldply(lapply(Files[31], read.csv, strip.white = TRUE))
+#Block <- ldply(lapply(Files[31], read.csv, strip.white = TRUE))[,-1]
 Data <- ldply(lapply(Files[1:30], read.csv, strip.white = TRUE))
 Data <- droplevels(subset(Data, eval(Level == "CD BLOCK")))[,-c(0:9)]
-Data <- droplevels(subset(Data, eval(is.element(Name, Block[,1]))))
+#Data <- FindReplace(data = Data, Var = "Name", replaceData = Block, from = "Old", to = "New", exact = TRUE)
 
-Data <- FindReplace(data = Data, Var = "Name", replaceData = Block, from = "Old", to = "New", exact = TRUE)
+#Data <- droplevels(subset(Data, eval(is.element(Name, Block[,1]))))
+
 Total <- droplevels(subset(Data, eval(Data$Total.Rural.Urban == "Total")))
 Total$SCST <- Total$Scheduled.Castes.population.Person + Total$Scheduled.Tribes.population.Person
 Total <- Total[, c(1, 3, 4, 7, 16, 22, 88)]
 colnames(Total) <- c("Block_Name", "Households", "TotPop", "Pop0-6", "Literates", "TotWPop", "SCST")
 
 #write.csv(Total, "Z1.csv")
+
+Files <- list.files(pattern = "*.csv")
+Data <- ldply(lapply(Files[31], read.csv, strip.white = TRUE))[,-1]
 Dataset <- read.csv("D:/Share Point/OneDrive/GitHub/greek-salad/timey-wimey.csv")
-Dataset <- droplevels(subset(Dataset, eval(Year == 2011)))
+Dataset <- droplevels(subset(Dataset, eval(Year == 2005)))
 #Dataset <- FindReplace(data = Dataset, Var = "Block_Name", replaceData = Block, from = "Old", to = "New", exact = TRUE)
-Dataset <- data.frame(merge(Dataset, Total, by = "Block_Name"))
+Dataset <- data.frame(merge(Dataset, Data, by = "Block_Name"))
+
+write.csv(Dataset, "D:/Share Point/OneDrive/GitHub/greek-salad/Z0-Baseline.csv")
--- Regressions.R
+++ Regressions.R
@@ -9,6 +9,8 @@ library(foreign)
 library(stargazer)
 library(plyr)
 library(reshape)
+library(MatchIt)
+library(optmatch)
 
 options(scipen = 999, digits=2)
 Files <- list.files(pattern = "*.csv")
@@ -19,19 +21,36 @@ na.zero <- function (x) {
   return(x)
 }
 
+#Propensity Score Matching
+Data <- ldply(lapply(Files[6], read.csv))[,-c(1, 3)]
+m.out <- matchit(Split ~ Total + Female_Tch + Electricity + Public + Graduate_Teachers + Households + Pop0.6 + Literates + SCST, data = Data, method = "nearest", ratio = 1, discard = "both")
+summary(m.out)
+#plot(m.out, type = "jitter")
+plot(m.out, type = "hist")
+
+match <- match.data(m.out)
+write.csv(match, "Z1-Matched.csv")
+
 Data <- ldply(lapply(Files[5], read.csv))[,-c(1, 29)]
 #The variable all.taluks is from the environment of CensusPrep.R - Run that first! (or read it in from Z2.csv)
-all.taluks <- ldply(lapply(Files[6], read.csv))[,2]
+all.taluks <- ldply(lapply(Files[7], read.csv))[,2]
 Data <- droplevels(subset(Data, eval(is.element(Block_Name, all.taluks))))
 #Summary Statistics
-
+#Baseline
 Summ <- aggregate(Data, by = list(Data$Year, Data$Split), mean)
 Summ <- t(droplevels(subset(Summ, eval(Year == 2005))))
 Summ <- Summ[-c(1:10, 12),]
 colnames(Summ) <- c("Undivided", "Divided")
-
 #Print the Summary Stats
 stargazer(Summ, title = "Summary Statistics in 2005", summary = NULL)
+
+#Endline
+Summ <- aggregate(Data, by = list(Data$Year, Data$Split), mean)
+Summ <- t(droplevels(subset(Summ, eval(Year == 2013))))
+Summ <- Summ[-c(1:10, 12),]
+colnames(Summ) <- c("Undivided", "Divided")
+#Print the Summary Stats
+stargazer(Summ, title = "Summary Statistics in 2013", summary = NULL)
 
 fit.total <- lm(Total ~ Split*Year, Data)
 stargazer(fit.total)
