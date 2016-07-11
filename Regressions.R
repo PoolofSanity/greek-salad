bf09aa8cbfa2ebb658fd737b28e7f92f57167f0b
Jothsna Rajan <jothsnarajan@gmail.com>
2016-07-11 21:31:10 +0530

    v2.3

--- Regressions.R
+++ Regressions.R
@@ -152,25 +152,46 @@ colnames(Summ) <- c("Old", "New")
 #Print the Summary Stats
 stargazer(Summ, title = "Summary Statistics in 2013", summary = NULL)
 
-fit.total <- lm(TotalMarks ~ Split*Year, Data)
-fit.WDays <- lm(WorkDays ~ Split*Year, Data)
-fit.AcdInsp <- lm(AcadInsp ~ Split*Year, Data)
-fit.DevGrant <- lm(DevGrantR ~ Split*Year, Data)
-stargazer(fit.total, fit.WDays, fit.DevGrant, fit.AcdInsp, digits = 1, df = FALSE)
+#Regression Models
+Files <- list.files(pattern = "*.csv")
+Data <- ldply(lapply(Files[c(6,8)], read.csv))[,-c(1, 3, 30)]
+colnames(Data) <- c("Block_Name", "Year", "Marks1", "Marks2", "Marks3", "Marks4", "Marks5", "Marks6", "TotalMarks", "Dist_Name", "Rural", "WorkDays",  "AcadInsp", "DevGrantR", "DevGrantE", "TLMGrantR", "TLMGrantE", "Classrooms", "ToiletG", "Electricity", "Library", "PlayGround", "Male_Tch", "Female_Tch", "Grad_Tch", "ProfQ_Tch", "Days_nonTch", "Public", "Split", "Households", "TotPop", "Pop0.6", "Literates", "TotWPop", "SCST", "District", "Yr2005", "Yr2006", "Yr2007", "Yr2008", "Yr2009", "Yr2010", "Yr2011", "Yr2012", "Yr2013", "SchoolperPop2005", "SchoolperPop2006", "SchoolperPop2007", "SchoolperPop2008", "SchoolperPop2009", "SchoolperPop2010", "SchoolperPop2011", "SchoolperPop2012", "SchoolperPop2013")
+Data$Post <- rep(0, nrow(Data))
+Data[Data$Year == 2013, 55] = 1
+Data$SchoolNo <- Data$SchoolperPop2013 - Data$SchoolperPop2005
 
-fit.TLM <- lm(TLMGrantR ~ Split*Year, Data)
-fit.Class <- lm(Classrooms ~ Split*Year, Data)
-fit.Toilet <- lm(ToiletG ~ Split*Year, Data)
-fit.Elect <- lm(Electricity ~ Split*Year, Data)
-stargazer(fit.TLM, fit.Class, fit.Toilet, fit.Elect, digits = 1, df = FALSE)
+#Funding related variables
+fit.School <- lm(SchoolNo ~ Split*Post, Data)
+fit.public <- lm(Public ~ Split*Post, Data)
+fit.DevGrant <- lm(DevGrantR ~ Split*Post, Data)
+fit.TLM <- lm(TLMGrantR ~ Split*Post, Data)
+stargazer(fit.School, fit.public, fit.DevGrant, fit.TLM, digits = 1, df = FALSE)
 
-fit.male <- lm(Male_Tch ~ Split*Year, Data)
-fit.fem <- lm(Female_Tch ~ Split*Year, Data)
-fit.grad <- lm(Grad_Tch ~ Split*Year, Data)
-fit.prof <- lm(ProfQ_Tch ~ Split*Year, Data)
+#Infrastructure related variables
+fit.Class <- lm(Classrooms ~ Split*Post + SchoolNo + Public + DevGrantR + TLMGrantR, Data)
+fit.Toilet <- lm(ToiletG ~ Split*Post + SchoolNo + Public + DevGrantR + TLMGrantR, Data)
+fit.Elect <- lm(Electricity ~ Split*Post + SchoolNo + Public + DevGrantR + TLMGrantR, Data)
+fit.libr <- lm(Library ~ Split*Post + SchoolNo + Public + DevGrantR + TLMGrantR, Data)
+stargazer(fit.Class, fit.Toilet, fit.Elect, fit.libr, digits = 1, df = FALSE)
+
+#Teaching resources related variables
+fit.male <- lm(Male_Tch ~ Split*Post + SchoolNo + Public + DevGrantR + TLMGrantR, Data)
+fit.fem <- lm(Female_Tch ~ Split*Post + SchoolNo + Public + DevGrantR + TLMGrantR, Data)
+fit.grad <- lm(Grad_Tch ~ Split*Post + SchoolNo + Public + DevGrantR + TLMGrantR, Data)
+fit.prof <- lm(ProfQ_Tch ~ Split*Post + SchoolNo + Public + DevGrantR + TLMGrantR, Data)
 stargazer(fit.male, fit.fem, fit.grad, fit.prof, digits = 1, df = FALSE)
 
-fit.public <- lm(Public ~ Split*Year, Data)
-fit.nonTch <- lm(Days_nonTch ~ Split*Year, Data)
-fit.libr <- lm(Library ~ Split*Year, Data)
-stargazer(fit.public, fit.nonTch, fit.libr, digits = 1, df = FALSE)
+#Organization related variables
+fit.WDays <- lm(WorkDays ~ Split*Post + SchoolNo + Public + DevGrantR + TLMGrantR, Data)
+fit.AcdInsp <- lm(AcadInsp ~ Split*Post + SchoolNo + Public + DevGrantR + TLMGrantR, Data)
+fit.nonTch <- lm(Days_nonTch ~ Split*Post + SchoolNo + Public + DevGrantR + TLMGrantR, Data)
+fit.scores <- lm(TotalMarks ~ Split*Post + SchoolNo + Public + DevGrantR + TLMGrantR, Data)
+stargazer(fit.WDays, fit.nonTch, fit.AcdInsp, fit.scores, digits = 1, df = FALSE)
+
+#time dependent Model
+Data <- ldply(lapply(Files[5], read.csv))[,-c(1)]
+School <- ldply(lapply(Files[9], read.csv))
+Dataset <- merge(Data, School, by = c("Block_Name", "Year"))
+
+Dataset$Post <- rep(0, nrow(Dataset))
+Dataset[Data$Year > 2010, 33] = 1
